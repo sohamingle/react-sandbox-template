fae9f22ff9f28624897023216b8db96cbe8e2e9a
Soham Ingle <sohamingle@gmail.com>
2025-12-09 23:03:00 +0530

    Update Vite server configuration to set host to '0.0.0.0' and allow all hosts

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,8 @@ import tailwindcss from '@tailwindcss/vite'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
-  server:{
-    allowedHosts: ['*'],
+  server: {
+    host: '0.0.0.0',
+    allowedHosts: true,
   }
 })
